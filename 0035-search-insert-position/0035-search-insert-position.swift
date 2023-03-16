c7f3d7a802f73b7c8afafb1d831f29bdadcfe48e
psychehose <61657569+psychehose@users.noreply.github.com>
2023-03-16 23:14:33 +0900

    Time: 24 ms (82.35%), Space: 14.3 MB (84.71%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.swift
+++ 0035-search-insert-position/0035-search-insert-position.swift
@@ -1,61 +1,18 @@
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        var leftIndex = 0
-        var rightIndex = nums.count - 1
-        var mid = 0
-        var lastIndex = 0
+        var left = 0, right = nums.count - 1
         
-        if nums[0] >= target {
-            return 0
-        }
-        if nums[rightIndex] < target {
-            return rightIndex + 1
-        }
-        
-        
-        while leftIndex <= rightIndex {
-            mid = leftIndex + (rightIndex - leftIndex) / 2
-            
-            if target == nums[mid] {
-                return mid
-            }
-            
-            else if target < nums[mid] {
-                rightIndex = mid - 1
-                lastIndex = mid
-            }
-            
-            else {
-                leftIndex = mid + 1
-                lastIndex = mid
-            }
-        }
-        
-     
-        // lastIndex 여기에서 어떻게 잘 넣어줘야할까의 문제   
-        
-        print("lastIndex ==", lastIndex)
-        
-        if lastIndex == 0 {
-            if nums[lastIndex] >= target {
-                return  0
+        while left <= right {
+            var pivot = left + (right - left) / 2
+            if nums[pivot] == target {
+                return pivot
+            } else if nums[pivot] > target {
+                right = pivot - 1
             } else {
-                return 1
+                left = pivot + 1
             }
         }
         
-        
-        else {
-            if nums[lastIndex] > target {
-                if nums[lastIndex - 1] < target {
-                    return lastIndex
-                }
-                return lastIndex - 1
-            } 
-            else {
-              
-                return lastIndex + 1
-            }
-        }
+        return left
     }
 }
