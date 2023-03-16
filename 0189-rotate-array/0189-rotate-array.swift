2cb7beab804572cd20df20e2e8c2d11c7faf5438
psychehose <61657569+psychehose@users.noreply.github.com>
2023-03-17 00:27:25 +0900

    Time: 936 ms (25.46%), Space: 21.5 MB (73.51%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        
+        var numsCopy = nums
+        
+        if k == 0 {
+            return
+        }
+        
+        for index in 0 ..< k {
+            var last = nums.removeLast()
+            nums.insert(last, at: 0)
+        }
+        
+        
+
+    
+        
+    }
+}
