17cf3c8207faf35383cd119f2c63beee1f645dd9
psychehose <61657569+psychehose@users.noreply.github.com>
2023-04-18 17:26:34 +0900

    Time: 92 ms (49.54%), Space: 14.2 MB (96.33%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.swift
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.swift
@@ -13,63 +13,20 @@
  *     }
  * }
  */
-
-extension Solution {
-    func addTree(_ a: TreeNode?, _ b: TreeNode?) -> TreeNode? {
-        if a == nil && b == nil {
-            return nil
-        }
-        
-        if a == nil {
-            return TreeNode(b!.val)
-        }
-        
-        else if b == nil {
-            return TreeNode(a!.val)
-        }
-        
-        else {
-            return TreeNode(a!.val + b!.val)
-        }
-    }
-}
 class Solution {
     func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
         
-        if root1 == nil && root2 == nil {
-            return nil
+        if root1 == nil {
+            return root2
         }
-
-        var head = addTree(root1, root2)
-        
-        func traverse(_ root: TreeNode?, _ root1: TreeNode?, _ root2: TreeNode?) {
-            
-            guard let node = root else {
-                return
-            }
-            
-            var left = addTree(root1?.left, root2?.left) // 4
-            var right = addTree(root1?.right, root2?.right) // 5
-
-            
-            node.left = left
-            node.right = right
-
-            traverse(node.left, root1?.left, root2?.left)
-            traverse(node.right, root1?.right, root2?.right)
+        else if root2 == nil {
+            return root1
         }
         
+        root1!.val += root2!.val
+        root1?.left = mergeTrees(root1?.left, root2?.left)
+        root1?.right = mergeTrees(root1?.right, root2?.right)
         
-        traverse(head, root1, root2)
-
-        
-        return head
+        return root1
     }
 }
-
-// 원소가 1개 -> level 1
-// 원소가 2개 or 3개 -> level 2
-// 원소가 4개 or 
-
-// newNode -> TreeNode
-// head -> 
