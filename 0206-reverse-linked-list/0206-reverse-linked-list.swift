a6c4b58849591a4256c95e7ae996d2deded87801
psychehose <61657569+psychehose@users.noreply.github.com>
2023-04-22 17:08:37 +0900

    Time: 12 ms (77.69%), Space: 15.2 MB (10.77%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.swift
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+
+        var copy = head
+        var list: [ListNode] = []
+        var result: ListNode? = ListNode()
+        var headCursor = result
+        
+        while var node = copy {
+            list.append(ListNode(node.val))
+            copy = copy?.next
+        }
+        while var node = list.popLast() {
+            result?.next = node
+            result = result?.next
+        }
+
+        return headCursor?.next
+
+    }
+}
