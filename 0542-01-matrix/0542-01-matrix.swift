28dee7fcc08f1bbf112dc658247286112bb7d002
psychehose <61657569+psychehose@users.noreply.github.com>
2023-04-29 01:38:14 +0900

    Time: 395 ms (65.59%), Space: 15.6 MB (46.24%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.swift
+++ 0542-01-matrix/0542-01-matrix.swift
@@ -1,72 +1,120 @@
 class Solution {
-    // Using BFS
-    func updateMatrix(_ mat: [[Int]]) -> [[Int]] {   
-        
+    
+    // Using dynamic programming
+
+    func updateMatrix(_ mat: [[Int]]) -> [[Int]] {
         var row = mat.count
         var col = mat[0].count
-        var mat_bfs: [[Int]] = Array(
-            repeating: Array(repeating: Int.max, count:col), count:row)
         
-        var queue: [(Int, Int)] = []
+        var mat_dp: [[Int]] = Array(
+            repeating: Array(repeating: Int.max - 1000000, count: col), count: row)
         
-        
+
         for i in 0 ..< row {
             for j in 0 ..< col {
                 if mat[i][j] == 0 {
-                    mat_bfs[i][j] = 0
-                    queue.append((i, j))
+                    mat_dp[i][j] = 0
                 }
-            }
-        }
-        
-        var dxdy: [(Int, Int)] = [(-1, 0), (1,0), (0, -1), (0, 1)]
-        
-        
-        while(!queue.isEmpty) {
-            var element = queue.removeFirst()
-            
-            for d in dxdy {
-                var newX = (element.0 + d.0)
-                var newY = (element.1 + d.1)
                 
-                if newX >= 0 && newX < row && newY >= 0 && newY < col {
-                    if mat_bfs[newX][newY] > mat_bfs[element.0][element.1] + 1 {
-                        mat_bfs[newX][newY] = mat_bfs[element.0][element.1] + 1
-                        queue.append((newX, newY))
+                else {
+                    if i > 0 {
+                        mat_dp[i][j] = min(mat_dp[i][j], mat_dp[i-1][j] + 1)
+                    }
+                    
+                    if j > 0 {
+                        mat_dp[i][j] = min(mat_dp[i][j], mat_dp[i][j-1] + 1)
                     }
                 }
-                
             }
-            
         }
         
         
+        for i in (0 ..< row).reversed() {
+            for j in (0 ..< col).reversed() {
+
+                
+
+                    if i < row - 1 {
+                        mat_dp[i][j] = min(mat_dp[i][j], mat_dp[i+1][j] + 1)
+                    }
+                    
+                    if j < col - 1 {
+                        mat_dp[i][j] = min(mat_dp[i][j], mat_dp[i][j+1] + 1)
+                    }
+            }
+        }
         
-        // 0 0 0  
-        // 0 1 0
-        // 1 1 1
-        
-        // 0 0 0  
-        // 0 M 0
-        // M M M
-        
-        // 0 0 0  
-        // 0 1 0
-        // M M M
-        
-        // 0 0 0  
-        // 0 1 0
-        // 1 M M
-        
-        // 0 0 0  
-        // 0 1 0
-        // 1 M 1
-        
-        // 0 0 0  
-        // 0 1 0
-        // 1 2 1
-        return mat_bfs
+        return mat_dp
     }
+    
+    
+    
+    
+//     // Using BFS
+//     func updateMatrix(_ mat: [[Int]]) -> [[Int]] {   
+        
+//         var row = mat.count
+//         var col = mat[0].count
+//         var mat_bfs: [[Int]] = Array(
+//             repeating: Array(repeating: Int.max, count:col), count:row)
+        
+//         var queue: [(Int, Int)] = []
+        
+        
+//         for i in 0 ..< row {
+//             for j in 0 ..< col {
+//                 if mat[i][j] == 0 {
+//                     mat_bfs[i][j] = 0
+//                     queue.append((i, j))
+//                 }
+//             }
+//         }
+        
+//         var dxdy: [(Int, Int)] = [(-1, 0), (1,0), (0, -1), (0, 1)]
+        
+        
+//         while(!queue.isEmpty) {
+//             var element = queue.removeFirst()
+            
+//             for d in dxdy {
+//                 var newX = (element.0 + d.0)
+//                 var newY = (element.1 + d.1)
+                
+//                 if newX >= 0 && newX < row && newY >= 0 && newY < col {
+//                     if mat_bfs[newX][newY] > mat_bfs[element.0][element.1] + 1 {
+//                         mat_bfs[newX][newY] = mat_bfs[element.0][element.1] + 1
+//                         queue.append((newX, newY))
+//                     }
+//                 }
+                
+//             }
+            
+//         }
+//         // 0 0 0  
+//         // 0 1 0
+//         // 1 1 1
+        
+//         // 0 0 0  
+//         // 0 M 0
+//         // M M M
+        
+//         // 0 0 0  
+//         // 0 1 0
+//         // M M M
+        
+//         // 0 0 0  
+//         // 0 1 0
+//         // 1 M M
+        
+//         // 0 0 0  
+//         // 0 1 0
+//         // 1 M 1
+        
+//         // 0 0 0  
+//         // 0 1 0
+//         // 1 2 1
+//         return mat_bfs
+//     }
     
     
     // Using Brute Force
