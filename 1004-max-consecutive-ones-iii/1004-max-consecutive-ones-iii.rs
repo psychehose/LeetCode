c847ccc96ffbaea6bb17180aee6fa7832a82a14f
psychehose <61657569+psychehose@users.noreply.github.com>
2023-05-24 13:53:48 +0900

    Time: 338 ms (10.00%), Space: 2.3 MB (14.00%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.rs
@@ -0,0 +1,38 @@
+impl Solution {
+    pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
+        
+        use std::cmp;
+
+        let mut res = 0;
+        let mut flip = k;
+        
+        let mut left = 0;
+        let mut right = 0;
+        
+        while right < nums.len() {
+            // right 이동
+            if flip > 0 {
+                if nums[right] == 0 {
+                    flip -= 1;
+                }
+                right += 1;
+            }
+            else {
+                if nums[right] == 0 {
+                    if nums[left] == 0 { flip += 1;}
+                    left += 1;
+                }
+                else {
+                    right += 1;
+                }
+            }
+            
+            let mut length = (right - left) as i32;
+            println!("{}", length);
+            res = cmp::max(res, length);
+        }
+        
+
+        res as i32
+    }
+}
