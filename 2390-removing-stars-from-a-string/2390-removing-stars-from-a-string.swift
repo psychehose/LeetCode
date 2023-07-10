8f2bb73eb599b38bbe73fe097f3e8adf363bae9b
psychehose <61657569+psychehose@users.noreply.github.com>
2023-07-10 18:42:03 +0900

    Time: 295 ms (77.65%), Space: 17.3 MB (62.35%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.swift
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.swift
@@ -1,10 +1,8 @@
 class Solution {
     func removeStars(_ s: String) -> String {
+
         var stack: [Character] = []
-        
-        var arr: Array<Character> = Array(s)
-        
-        
+
         for c in s {
             if  c != "*" {
                 stack.append(c)
