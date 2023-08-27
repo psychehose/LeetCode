347a98c6f8d710a7acced49e08a36b325513596f
psychehose <61657569+psychehose@users.noreply.github.com>
2023-08-28 01:29:50 +0900

    Time: 83 ms (89.93%), Space: 49.6 MB (34.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -14,216 +14,23 @@ public class Solution {
         
         ListNode prehead = new ListNode();
         ListNode added = prehead;
+        int carry = 0;
         
-        bool isUp = false;
-        
-        while((l1 != null && l2 != null))  {
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 =  (l1 != null) ? l1.val : 0;
+            int digit2 =  (l2 != null) ? l2.val : 0;
             
-            if (isUp) {
-                // 전에서 10이 넘은 경우 1을 더해줌
-                
-                int val = l1.val + l2.val + 1;
-                
-                if(val >= 10) {
-                    // 10 넘은경우
-                    isUp = true;
-                    val = val - 10;
-                    
-                } else {
-                    // 10 넘지 않은 경우
-                    isUp = false;
-                }
-                
-                ListNode next = new ListNode(val);
-                added.next = next;
-                added = added.next;
-
-            }
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
             
-            else {
-                // 전에서 10이 넘지 않은 경우
-                int val = l1.val + l2.val;
-
-                if(val >= 10) {
-                    // 10 넘은경우
-                    isUp = true;
-                    val = val - 10;
-                    
-                } else {
-                    // 10 넘지 않은 경우
-                    isUp = false;
-                }
-                
-                ListNode next = new ListNode(val);
-                added.next = next;
-                added = added.next;
-            }
-            
-            l1 = l1.next;
-            l2 = l2.next;
-
-        }
-        
-        // IsUp 처리와 나머지 L1, L2 나머지 처리
-        
-        if (isUp) {
-            // 1올려주기
-            if(l1 != null || l2 != null) {
-                if(l1 != null) {
-                    // l1이 null이 아님
-                    while(l1 != null) {
-                        if (isUp) {
-                            int val = l1.val + 1;
-                            
-                            if(val >= 10) {
-                                // 10 넘은 경우
-                                val = val - 10;
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = true;
-                                
-                            } else {
-                                // 10 넘지 않은 경우
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = false;
-                            }
-                            
-                        } else {
-                            int val = l1.val;
-                            ListNode next = new ListNode(val);
-                            added.next = next;
-                            added = added.next;
-                            isUp = false;
-                        }
-                        l1 = l1.next;
-                    }
-                }
-                
-                else {
-                    while(l2 != null) {
-                        if (isUp) {
-                            int val = l2.val + 1;
-                            
-                            if(val >= 10) {
-                                // 10 넘은 경우
-                                val = val - 10;
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = true;
-                                
-                            } else {
-                                // 10 넘지 않은 경우
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = false;
-                            }
-                            
-                        } else {
-                            int val = l2.val;
-                            ListNode next = new ListNode(val);
-                            added.next = next;
-                            added = added.next;
-                            isUp = false;
-                        }
-                        l2 = l2.next;
-                    }
-                }
-                
-                
-            } else {
-                //
-                ListNode next = new ListNode(1);
-                added.next = next;
-                added = added.next;
-                isUp = false;
-                
-            }
-        }
-        
-        else {
-            if(l1 != null || l2 != null) {
-                if(l1 != null) {
-                    // l1이 null이 아님
-                    while(l1 != null) {
-                        if (isUp) {
-                            int val = l1.val + 1;
-                            
-                            if(val >= 10) {
-                                // 10 넘은 경우
-                                val = val - 10;
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = true;
-                                
-                            } else {
-                                // 10 넘지 않은 경우
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = false;
-                            }
-                            
-                        } else {
-                            int val = l1.val;
-                            ListNode next = new ListNode(val);
-                            added.next = next;
-                            added = added.next;
-                            isUp = false;
-                        }
-                        l1 = l1.next;
-                    }
-                }
-                
-                else {
-                    while(l2 != null) {
-                        if (isUp) {
-                            int val = l2.val + 1;
-                            
-                            if(val >= 10) {
-                                // 10 넘은 경우
-                                val = val - 10;
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = true;
-                                
-                            } else {
-                                // 10 넘지 않은 경우
-                                ListNode next = new ListNode(val);
-                                added.next = next;
-                                added = added.next;
-                                isUp = false;
-                            }
-                            
-                        } else {
-                            int val = l2.val;
-                            ListNode next = new ListNode(val);
-                            added.next = next;
-                            added = added.next;
-                            isUp = false;
-                        }
-                        l2 = l2.next;
-                    }
-                }
-            } else {
-                // Nothing
-            }
-        }
-        
-        
-        if (isUp) {
-            ListNode next = new ListNode(1);
+            ListNode next = new ListNode(digit);
             added.next = next;
             added = added.next;
-        }
-        
-        
+            
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        } 
         
         return prehead.next;
         
