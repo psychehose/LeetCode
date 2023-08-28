e8bd4274016e24cb82ebd7b203ee3d9cc966764a
psychehose <61657569+psychehose@users.noreply.github.com>
2023-08-28 15:37:45 +0900

    Time: 337 ms (13.41%), Space: 123.4 MB (5.01%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cs
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cs
@@ -14,10 +14,12 @@ public class Solution {
         
         ListNode merged = null;
 
+        // Array 순회
         for (int i = 0; i < lists.Length; i++) {
             
             ListNode poped = lists[i];
 
+            // Poped와 merged를 합쳐서 cur로 만들고 merged를 업데이트 해주기.
             ListNode root = new ListNode(-1000000);
             ListNode cur = root;
             
@@ -25,32 +27,34 @@ public class Solution {
             
             while(poped != null || merged != null) {
                 
-                // ListNode가 Empty인 경우 나머지 노드를 넥스트로 쭉 합친다
+                // 하나의 ListNode가 Null인 경우 나머지 노드를 넣는다.
                 if (poped == null) {
                     cur.next = merged;
                     cur = cur.next;
                     break;
                 }
-                if (merged == null) {
+                else if (merged == null) {
                     cur.next = poped;
                     cur = cur.next;
                     break;
                 }
+                
                 // ListNode가 Empty가 아닌 경우
-                if (cur.next == null) {
+                else {
                     if(poped.val >= merged.val) {
                         // merged.val을 넥스트로
                         ListNode next = new ListNode(merged.val);
                         cur.next = next;
                         merged = merged.next;
+                        cur = cur.next;
                     } else {
                         // poped.val을 넥스트로
                         ListNode next = new ListNode(poped.val);
                         cur.next = next;
                         poped = poped.next;
+                        cur = cur.next;
                     }   
                 }
-                cur = cur.next;
             }
             // cur가 다 합쳐진 merged -> merged에 넣자.
             merged = root.next;
