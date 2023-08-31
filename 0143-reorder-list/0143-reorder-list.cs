69eece007529cfc50b09e2207091812333279d72
psychehose <61657569+psychehose@users.noreply.github.com>
2023-08-31 23:10:49 +0900

    Time: 470 ms (5.05%), Space: 42.7 MB (61.05%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cs
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public void ReorderList(ListNode head) {
+        
+        if (head == null || head.next == null) {
+            return;
+        }
+        
+        ListNode cur = head;
+        
+        int len = 0;
+
+        // Get Node len 
+        
+        while(cur != null) {
+            len++;
+            cur = cur.next;
+        }
+        cur = head;
+
+        int ord = -1;
+        
+        while(cur != null) {
+            
+            ord +=2;
+
+            ListNode before = cur;
+            
+            if (len - ord < 2) {
+                break;
+            }
+
+            for (int i = 1; i < len - ord; i++) {
+                before = before.next;
+            }
+            
+            ListNode reorder = before.next;
+            before.next = null;
+
+            reorder.next = cur.next;
+            cur.next = reorder;
+            
+            cur = cur.next.next;
+        }
+        
+        
+        
+        return;
+        
+        
+    }
+}
